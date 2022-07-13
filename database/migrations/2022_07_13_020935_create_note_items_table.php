e55d31a795ff986d249aca7a70b3ad7788f1df13
Juan Salazar <jsalazarv@MacBook-Pro-de-Juan.local>
2022-07-12 21:26:13 -0500

    Added migrations

---
+++ database/migrations/2022_07_13_020935_create_note_items_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('note_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('note_id')
+                ->nullable()
+                ->constrained('notes')
+                ->nullOnDelete();
+            $table->foreignId('item_id')
+                ->nullable()
+                ->constrained('items')
+                ->nullOnDelete();
+            $table->integer('quantity');
+            $table->integer('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('note_items');
+    }
+};
